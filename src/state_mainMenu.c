8f5f1f530998926da61579d8bfeba0447508322f
SirGFM <gfmandaji@hotmail.com>
2015-07-29 22:20:43 -0300

    Added background and title animation

--- src/state_mainMenu.c
+++ src/state_mainMenu.c
@@ -15,14 +15,25 @@
 #include <stdlib.h>
 #include <string.h>
 
+/** Title data for the tilemap */
 int titleData[] = {
 #include "../assets/title.csv"
+};
+/** Backgroun data for the tilemap */
+int backgroundData[] = {
+#include "../assets/background.csv"
 };
 
 /** Main menu struct */
 struct stMainMenuCtx {
     /** A tilemap to show the title, in 8x8 tiles */
     gfmTilemap *pTitle;
+    /** A tilemap to show the background, in 8x8 tiles */
+    gfmTilemap *pBackground;
+    /** Current position in the "title animation" (copying of the tilemap) */
+    int titleAnimation;
+    /** Accumulator for the animation */
+    int animationTime;
 };
 
 /**
@@ -33,7 +44,6 @@ struct stMainMenuCtx {
  */
 static gfmRV state_mainMenu_init(gameCtx *pGame) {
     gfmRV rv;
-    int i;
     mainMenuCtx *pMainMenu;
     
     // Sanitize arguments
@@ -46,23 +56,22 @@ static gfmRV state_mainMenu_init(gameCtx *pGame) {
     // Clean it
     memset(pMainMenu, 0x0, sizeof(mainMenuCtx));
     
-    // Fix the tile data
-    i = 0;
-    while (i < sizeof(titleData) / sizeof(int)) {
-        if (titleData[i] == 49) {
-            titleData[i] = -1;
-        }
-        else {
-            titleData[i] += 64;
-        }
-        i++;
-    }
     // Alloc and initialize the title
     rv = gfmTilemap_getNew(&(pMainMenu->pTitle));
     ASSERT_NR(rv == GFMRV_OK);
     rv = gfmTilemap_init(pMainMenu->pTitle, pGame->pSset8x8, 32, 16, -1);
     ASSERT_NR(rv == GFMRV_OK);
-    rv = gfmTilemap_loadStatic(pMainMenu->pTitle, titleData, 32, 16);
+    rv = gfmTilemap_setPosition(pMainMenu->pTitle, 32, 0);
+    ASSERT_NR(rv == GFMRV_OK);
+    
+    // Alloc and load the background
+    rv = gfmTilemap_getNew(&(pMainMenu->pBackground));
+    ASSERT_NR(rv == GFMRV_OK);
+    rv = gfmTilemap_init(pMainMenu->pBackground, pGame->pSset8x16, 40, 8, -1);
+    ASSERT_NR(rv == GFMRV_OK);
+    rv = gfmTilemap_loadStatic(pMainMenu->pBackground, backgroundData, 40, 8);
+    ASSERT_NR(rv == GFMRV_OK);
+    rv = gfmTilemap_setPosition(pMainMenu->pBackground, 0, 56);
     ASSERT_NR(rv == GFMRV_OK);
     
     // TODO initialize everything else
@@ -88,6 +97,8 @@ static gfmRV state_mainMenu_clean(gameCtx *pGame) {
     
     // Clean the title
     gfmTilemap_free(&(pGame->pMainMenu->pTitle));
+    // Clean the background
+    gfmTilemap_free(&(pGame->pMainMenu->pBackground));
     // TODO Clean everything
     
     // Dealloc and clean the main menu
@@ -105,7 +116,46 @@ __ret:
  * @param  pGame The game "global" context
  */
 static gfmRV state_mainMenu_update(gameCtx *pGame) {
-    return GFMRV_OK;
+    gfmRV rv;
+    int elapsed;
+    mainMenuCtx *pMainMenu;
+    
+    // Sanitize arguments
+    ASSERT(pGame, GFMRV_ARGUMENTS_BAD);
+    ASSERT(pGame->pMainMenu, GFMRV_ARGUMENTS_BAD);
+    // Retrieve the main menu context
+    pMainMenu = pGame->pMainMenu;
+    
+    // Get how long elapsed since the last frame
+    rv = gfm_getElapsedTime(&elapsed, pGame->pCtx);
+    ASSERT_NR(rv == GFMRV_OK);
+    
+    pMainMenu->animationTime += elapsed;
+    while (pMainMenu->animationTime > 16) {
+        // Get the tile data and animate it
+        if (pMainMenu->titleAnimation < sizeof(titleData) / sizeof(int)) {
+            int *pData;
+            
+            // Go to the next valid (i.e., non-invisible) tile
+            while (pMainMenu->titleAnimation + 1 < sizeof(titleData) &&
+                    titleData[pMainMenu->titleAnimation] == -1) {
+                pMainMenu->titleAnimation++;
+            }
+            
+            // Get its data
+            rv = gfmTilemap_getData(&pData, pMainMenu->pTitle);
+            ASSERT_NR(rv == GFMRV_OK);
+            // Update the tile
+            pData[pMainMenu->titleAnimation] = titleData[pMainMenu->titleAnimation];
+            
+            pMainMenu->titleAnimation++;
+        }
+        pMainMenu->animationTime -= 16;
+    }
+    
+    rv = GFMRV_OK;
+__ret:
+    return rv;
 }
 
 /**
@@ -117,8 +167,16 @@ static gfmRV state_mainMenu_draw(gameCtx *pGame) {
     gfmRV rv;
     mainMenuCtx *pMainMenu;
     
+    // Sanitize arguments
+    ASSERT(pGame, GFMRV_ARGUMENTS_BAD);
+    ASSERT(pGame->pMainMenu, GFMRV_ARGUMENTS_BAD);
+    // Retrieve the main menu context
     pMainMenu = pGame->pMainMenu;
     
+    // Draw the background
+    rv = gfmTilemap_draw(pMainMenu->pBackground, pGame->pCtx);
+    ASSERT_NR(rv == GFMRV_OK);
+    // Draw the title
     rv = gfmTilemap_draw(pMainMenu->pTitle, pGame->pCtx);
     ASSERT_NR(rv == GFMRV_OK);
     
